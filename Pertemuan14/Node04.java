3180460cad1af1236bce7c3dcfff89262a5097e1
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-06-10 09:28:50 +0700

    Praktikum 1

---
+++ Pertemuan14/Node04.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node04 {
+    Mahasiswa04 mahasiswa;
+    Node04 left, right;
+
+    public Node04(){
+
+    }
+
+    public Node04(Mahasiswa04 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
